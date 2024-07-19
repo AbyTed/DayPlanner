0dbf237345e6a7dd9f89220583ef21c939f65a81
AbyTed <119811665+AbyTed@users.noreply.github.com>
2024-07-19 19:10:58 -0400

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -1,27 +1,40 @@
 data = {
-    'khan academy':(5),
-    'reading':(2),
+    'khan academy':(60, 'h'),
+    'reading':(60, 'e'),
     'break':(15),
-    'time':('1:00pm','5:00pm')
     
 }
 
 def day_planner(data):
-    store_activity = set()
-    TIME_SPAN = military_time(data['time']) 
+    easy = []
+    medium = []
+    hard = []
+    for activity in data:
+        difficulty = data[activity][1]
+        PAYLOAD = (activity, data[activity][0]) 
+        if difficulty == 'h':
+            hard.append(PAYLOAD)
+        elif difficulty == 'm':
+            medium.append(PAYLOAD)
+        else:
+            easy.append(PAYLOAD)
+    
+def sort_activity(easy, medium, hard):
+    
+    pass
+
+def assembler(result):
+    pass
+
+def display_schedule(schedule):
+    pass
         
-def military_time(time):
-    result = []
-    REMOVE_LENGTH = -2
-    TIME = time[0][:REMOVE_LENGTH]
-    TIME2 = time[1][:REMOVE_LENGTH]
-    if time[0].count('pm') == 1:
-       times_split = TIME.split(':')
-       
-       result.append(time[0][:REMOVE_LENGTH])
-    elif time[0].count('am') == 1:
-        result.append(time[0][:REMOVE_LENGTH])
-    return 0
+        
+        
+        
+        
+        
+
     
         
         
